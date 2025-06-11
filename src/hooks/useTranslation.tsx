5ebf51c9c7c247e8a835e22998f5c3a48bc9618f
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-06-11 14:08:48 +0300

    Update useTranslation.tsx

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -1,10 +1,15 @@
-import i18n from 'i18n-js';
+import { I18n } from 'i18n-js';
 import * as Localization from 'expo-localization';
 import Storage from '@react-native-async-storage/async-storage';
 import React, {useCallback, useContext, useEffect, useState} from 'react';
 
 import translations from '../constants/translations/';
 import {ITranslate} from '../constants/types';
+
+// Create a new i18n instance
+const i18n = new I18n(translations);
+i18n.defaultLocale = 'en';
+i18n.enableFallback = true;
 
 export const TranslationContext = React.createContext({});
 
@@ -15,16 +20,12 @@ export const TranslationProvider = ({
 }) => {
   const [locale, setLocale] = useState('en');
 
-  // Set the locale once at the beginning of your app.
+  // Set the locale for the i18n instance
   i18n.locale = locale;
-  // Set the key-value pairs for the different languages you want to support.
-  i18n.translations = translations;
-  // When a value is missing from a language it'll fallback to another language with the key present.
-  i18n.fallbacks = true;
 
   const t = useCallback(
-    (scope: i18n.Scope, options?: i18n.TranslateOptions) => {
-      return i18n.t(scope, {...options, locale});
+    (scope: string | string[], options?: object) => {
+      return i18n.translate(scope, {...options, locale});
     },
     [locale],
   );
